effc9d912ed283a6a06bf216c86c42a7cc13fc18
Mitke na www <sysadmin@udk.bg.ac.rs>
2024-04-02 08:57:07 +0200

    greška u scriptu za dodavanje dana

--- rezervacije/urls.py
+++ rezervacije/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('administracija/', admin.site.urls),
     #path('bookingRooms', include('bookingRooms/urls')),
     path('', include('booking.urls')),
 ]
