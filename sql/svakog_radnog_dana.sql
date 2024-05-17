efb05e89bc0f1690864b15396eeea5b316b49b1d
Mitke na www <sysadmin@udk.bg.ac.rs>
2024-05-17 09:12:06 +0200

    skinuo predavanje na hirurgiji

--- sql/svakog_radnog_dana.sql
+++ sql/svakog_radnog_dana.sql
@@ -1,15 +1,17 @@
 -- UČIONICA
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'jut. sastanak', 'svakog radnog dana', 1, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('Sofija', 'edukacija',  'svakog radnog dana', 1, 2, 'DATE 12:00:00', 'DATE 13:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'jut. sastanak', 'svakog radnog dana', 1, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('Sofija', 'edukacija',  'svakog radnog dana', 1, 2, 'DATE 12:00:00', 'DATE 13:00:00' );
 -- SALA-D
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'jut.sastanak', 'svakog radnog dana', 2, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'predavanja',  'svakog radnog dana', 2, 2, 'DATE 10:15:00', 'DATE 11:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'jut.sastanak', 'svakog radnog dana', 2, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
+-- nema do sledeće školske godine
+--INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'predavanja',  'svakog radnog dana', 2, 2, 'DATE 10:15:00', 'DATE 11:00:00' );
 -- SALA-L
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'jut.sastanak', 'svakog radnog dana', 3, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'predavanja',  'svakog radnog dana', 3, 2, 'DATE 10:15:00', 'DATE 11:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'jut.sastanak', 'svakog radnog dana', 3, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
+-- nema do sledeće školske godine
+--INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'predavanja',  'svakog radnog dana', 3, 2, 'DATE 10:15:00', 'DATE 11:00:00' );
 -- AMFITEATAR
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'uvodni čas', 'svakog radnog dana', 4, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'rezervisano', 'svakog radnog dana', 4, 2, 'DATE 08:15:00', 'DATE 10:00:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('fizikalna med.', 'rezervisano', 'svakog radnog dana', 4, 2, 'DATE 10:00:00', 'DATE 11:00:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'predavanje', 'svakog radnog dana', 4, 2, 'DATE 11:00:00', 'DATE 12:30:00' );
-insert into booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'predavanje spec.', 'svakog radnog dana', 4, 2, 'DATE 12:30:00', 'DATE 14:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'uvodni čas', 'svakog radnog dana', 4, 2, 'DATE 07:30:00', 'DATE 08:15:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('hirurgija', 'rezervisano', 'svakog radnog dana', 4, 2, 'DATE 08:15:00', 'DATE 10:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('fizikalna med.', 'rezervisano', 'svakog radnog dana', 4, 2, 'DATE 10:00:00', 'DATE 11:00:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'predavanje', 'svakog radnog dana', 4, 2, 'DATE 11:00:00', 'DATE 12:30:00' );
+INSERT INTO booking_bookings (organizer_name, purpose, napomena, room_id, user_id, start_time, end_time) VALUES ('pedijatrija', 'predavanje spec.', 'svakog radnog dana', 4, 2, 'DATE 12:30:00', 'DATE 14:00:00' );
